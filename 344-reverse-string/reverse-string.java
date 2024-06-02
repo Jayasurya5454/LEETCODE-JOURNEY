ea80044a32a74531c11c88f6a7270395001a41ba
JAYASURYA MUTHUKUMAR <123261802+Jayasurya5454@users.noreply.github.com>
2024-06-02 10:42:23 +0530

    Time: 128 ms (7.44%) | Memory: 46.5 MB (73.79%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        for(int i =0,j=s.length-1 ;i<=j;i++,j--)
+        {
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]=temp;
+        }
+        for(int i =0;i<s.length;i++)
+        {
+            System.out.println(s[i]);
+        }
+        
+    }
+}
