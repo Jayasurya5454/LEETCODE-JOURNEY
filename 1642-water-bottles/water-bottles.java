c27435e1f7ad552aa6640e7f7c958c3f73d67048
JAYASURYA MUTHUKUMAR <123261802+Jayasurya5454@users.noreply.github.com>
2024-07-22 22:26:29 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (96.57%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int sum=numBottles;
+
+            while(numBottles>=numExchange)
+            {
+                int k = numBottles/numExchange;
+                sum+=k;
+                numBottles = k+ (numBottles % numExchange);
+            }
+            return sum;
+            
+    }
+}
