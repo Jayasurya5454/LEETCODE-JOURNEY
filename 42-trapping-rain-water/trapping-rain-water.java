2f3de5b5be50a90f38bbdc1d912d4af8f7c4e41d
JAYASURYA MUTHUKUMAR <123261802+Jayasurya5454@users.noreply.github.com>
2024-12-27 18:48:03 +0530

    Time: 1 ms (64.81%) | Memory: 46.2 MB (57.42%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,26 +1,17 @@
 class Solution {
     public int trap(int[] h) {
-        int n = h.length;
-        if(n ==1)return 0;
+        int sum =0,n=h.length;;
         int larr[]=new int[n];
         int rarr[]=new int[n];
         larr[0]=h[0];
         rarr[n-1]=h[n-1];
-        for(int i=1;i<n;i++)
-        {
-            larr[i]=Math.max(h[i],larr[i-1]);
-        }
-        for(int i =n-2;i>=0;i--)
-        {
-            rarr[i]=Math.max(h[i],rarr[i+1]);
-
-        }
-        int sum =0;
+        for(int i= 1;i<n;i++)larr[i]=Math.max(larr[i-1],h[i]);
+        for(int i = n-2;i>=0;i--)rarr[i]=Math.max(rarr[i+1],h[i]);
         for(int i =0;i<n;i++)
         {
-           sum+= Math.min(larr[i],rarr[i])-h[i];
+            sum+= Math.min(larr[i],rarr[i])-h[i];
         }
         return sum;
-     
-}
+        
+    }
 }
