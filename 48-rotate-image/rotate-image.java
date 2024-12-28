2516dbdeb233a24fd31f031bdf4f9ceb1d793126
JAYASURYA MUTHUKUMAR <123261802+Jayasurya5454@users.noreply.github.com>
2024-12-28 10:22:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (96.98%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -16,9 +16,8 @@ class Solution {
             for(int j =0;j<rowcol/2;j++)
             {
                 temp = m[i][j];
-               m[i][j]=m[i][rowcol-j-1];
-               m[i][rowcol-j-1]=temp;
-                System.out.println(m[i][j]);
+                m[i][j]=m[i][rowcol-j-1];
+                 m[i][rowcol-j-1]=temp;
 
             }
         }
